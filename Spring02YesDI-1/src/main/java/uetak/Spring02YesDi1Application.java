718e3a65f7ab2bcc9054fc904b134903c9914737
uetak330 <uetak330@gmail.com>
2024-09-29 15:54:16 +0900

    DI有りのプロジェクト作成

---
+++ Spring02YesDI-1/src/main/java/uetak/Spring02YesDi1Application.java
@@ -0,0 +1,13 @@
+package uetak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring02YesDi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring02YesDi1Application.class, args);
+	}
+
+}
---
+++ Spring02YesDI-1/src/test/java/uetak/Spring02YesDi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package uetak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring02YesDi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
