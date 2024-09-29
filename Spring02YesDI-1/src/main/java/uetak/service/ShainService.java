c1ccfbbb019d1cb11a61b94ec7dff0121628298b
uetak330 <uetak330@gmail.com>
2024-09-29 16:00:44 +0900

    DI前のソースファイル作成

---
+++ Spring02YesDI-1/src/main/java/uetak/service/ShainService.java
@@ -0,0 +1,27 @@
+package uetak.service;
+
+import java.util.ArrayList;
+
+import uetak.entity.Shain;
+import uetak.repository.ShainRepository;
+
+/**
+ * サービス層: ビジネスロジックを実装する役割。 
+ * リポジトリとの連携: ShainRepositoryを使って、
+ * データベースやデータソースから社員情報を取得する。
+*/
+
+public class ShainService {
+	//社員リストを取得
+	public ArrayList<Shain> findAll() {
+		
+		//社員リポジトリのインスタンス作成
+		ShainRepository shainRepository = new ShainRepository();
+		
+		
+		
+		//社員リストを取得して返す
+		return shainRepository.findAll();
+	}
+	
+}
