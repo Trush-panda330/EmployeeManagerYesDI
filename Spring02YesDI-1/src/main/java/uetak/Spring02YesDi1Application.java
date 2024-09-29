cbf5c28166bacd1126472c646f5f769cc0bdda15
uetak330 <uetak330@gmail.com>
2024-09-29 16:20:52 +0900

    エントリークラスの訂正

--- Spring02YesDI-1/src/main/java/uetak/Spring02YesDi1Application.java
+++ Spring02YesDI-1/src/main/java/uetak/Spring02YesDi1Application.java
@@ -2,6 +2,9 @@ package uetak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import uetak.controller.ShainController;
 
 @SpringBootApplication
 public class Spring02YesDi1Application {
@@ -9,7 +12,14 @@ public class Spring02YesDi1Application {
 	//このmainメソッドを含むクラスと同じ階層にあるものだけが
 	//DIコンテナ内で影響される
 	public static void main(String[] args) {
-		SpringApplication.run(Spring02YesDi1Application.class, args);
+		//DIコンテナを作成
+		ConfigurableApplicationContext context = SpringApplication.run(Spring02YesDi1Application.class, args);
+
+		//社員コントローラーのインスタンスを呼ぶ
+		ShainController shainController = context.getBean(ShainController.class);
+
+		//インスタンスからメソッドを実行
+		shainController.findAll();
 	}
 
 }
