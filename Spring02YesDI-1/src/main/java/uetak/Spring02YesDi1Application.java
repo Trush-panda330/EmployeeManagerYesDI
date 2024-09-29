6623e54eed7fbef3cdfcf48877df263bc6b7258d
uetak330 <uetak330@gmail.com>
2024-09-29 16:03:39 +0900

    コメントの追記

--- Spring02YesDI-1/src/main/java/uetak/Spring02YesDi1Application.java
+++ Spring02YesDI-1/src/main/java/uetak/Spring02YesDi1Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring02YesDi1Application {
 
+	//このmainメソッドを含むクラスと同じ階層にあるものだけが
+	//DIコンテナ内で影響される
 	public static void main(String[] args) {
 		SpringApplication.run(Spring02YesDi1Application.class, args);
 	}
